3bcb7158641348b66fbfa017a0e2f4fdace50c45
Rohit Singh <154702706+RohitSingh1999@users.noreply.github.com>
2024-03-11 23:02:47 +0530

    Add files via upload

---
+++ variable_investigation.js
@@ -0,0 +1,30 @@
+let email = "rohitsingh2371999@gmail.com";
+const rohit = "7084220301";
+var password = "12564";
+let address = "lucknow";
+
+
+//rohit="152152"// it is not allow to redeclearing of any constant
+
+ email ="ron@gamil.com";
+password="486139";
+address="ghazipur";
+let accountState;
+//console.table([email,  password, rohit,  address, accountState]);
+console.log(password)
+console.log(email);
+console.log(address);
+console.log(rohit);
+//console.table([email, password, rohit ])
+console.log("hello rohit singh")
+
+let nandini= "sharma";
+let samrat="singh";
+let hello="hii";
+
+console.table([nandini, samrat, hello]);
+
+
+
+
+
