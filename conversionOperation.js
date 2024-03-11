f731d6764b1845534f6c01ae0b23ccc77d77b66d
Rohit Singh <154702706+RohitSingh1999@users.noreply.github.com>
2024-03-11 23:05:39 +0530

    Add files via upload

---
+++ conversionOperation.js
@@ -0,0 +1,88 @@
+const { log } = require("console");
+
+ let score = "125";
+ //console.log(score);
+
+ //console.log(typeof score);
+ //console.log(typeof(score));
+
+ let change = Number(score);
+ //console.log(typeof change);
+
+ let hello = 5469;
+ //console.log(typeof(hello));
+
+ let phtm = String(hello)
+ //console.log(phtm);
+ //console.log(typeof(phtm));
+
+  let hii = 0;
+ let booleancon = Boolean(hii);
+ //console.log(booleancon);
+
+ let nandini = "hello";
+ //console.log(typeof nandini);
+
+  // **************opreation************
+ //let singh = "number";
+ //console.log(singh);
+
+console.log(2+2/2);
+console.log("2"+2);
+console.log("2"+2*2);
+console.log("2"+2+2);
+console.log("2"+2/2);
+console.log("2"+2-2);
+console.log(2+2+"2");
+
+console.log("rohit"+"singh");
+
+let a, b, c;
+a=10;
+b=50;
+c=a*b;
+console.log(" add of two nomber "+c);
+
+let mynumber = 105;
+mynumber--;
+console.log("output "+(mynumber));
+ 
+let rohit = 647;
+rohit++;
+console.log((rohit))
+
+  function myfuncation(){
+    let a, b, c;
+    a= 38;
+    b=38;
+    c=a*b;
+    return c;
+
+ }
+ console.log(myfuncation())
+
+
+ let dhoni = 7;
+ //console.log(typeof(dhoni));
+ let sachin = String(dhoni);
+ console.log(typeof(sachin));
+
+ let rohu = function(){
+  a=50;
+  b=849;
+  c=a+b;
+  return c;
+ }
+ console.log(rohu());
+
+ const rajesh = ()=>{
+  a="kohli"
+  b="rohit"
+  c=a+b;
+  return c;
+ }
+ console.log(rajesh());
+
+
+
+
