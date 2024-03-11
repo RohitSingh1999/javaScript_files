d794b57c2e6dda868caa3f931e741dabdd4a7bf4
Rohit Singh <154702706+RohitSingh1999@users.noreply.github.com>
2024-03-11 23:05:00 +0530

    Add files via upload

---
+++ data_type.js
@@ -0,0 +1,61 @@
+"use Strict";//treat all js code as newer verson
+
+//alert(3+3);// not allow in node js it is not a browser.
+
+console.log(3+
+    3);// code readablity should be high
+
+    //console.log("rohit"); 
+
+    let rohit = "nandini"; // string
+    let king = 234; //number
+    let what = false;// boolean
+    let hello;   // undefined
+    let state = null;
+
+    // number => 2to power 53
+    // string => ""
+    // bigint 
+    // boolean => true/false
+    // null => standlone value
+    // undefined =>
+    // symbol => unique
+
+    // object
+
+    console.log(typeof rohit);
+    console.log(typeof undefined );
+    console.log(typeof null);
+    console.log(typeof what);
+    console.log(typeof state);
+    console.log(typeof king);
+
+
+let rahul= "hello";
+console.log(typeof rahul);
+
+const shaktiman =false;
+console.log(typeof shaktiman);
+
+let kohli;
+console.log(typeof kohli);
+
+let nandini = "2344";
+console.log(typeof nandini);
+
+const abhisek=[5, 6]
+console.log(typeof abhisek);
+
+const ankit ={
+    name: 'rahul singh',
+    roll: '274284294',
+    address: 'ghazipur'
+}
+console.log(typeof ankit);
+ let golu = Date();
+ console.log(golu);
+ console.log(typeof golu);
+ const number = 37883433438438488837n;
+ console.log(typeof number);
+
+
