a2b20d7f776a633e224fb33ed79a1b64755d0254
Rohit Singh <154702706+RohitSingh1999@users.noreply.github.com>
2024-03-11 23:10:49 +0530

    Add files via upload

---
+++ 01_funcation.js
@@ -0,0 +1,109 @@
+function sayMyName(){
+   console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+ sayMyName()
+
+ function addTwoNumbers(number1, number2){
+  number1=1;
+  number2=40;
+  return number1+number2
+  
+    
+
+     
+ }
+ addTwoNumbers()
+ 
+ 
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+//function loginUserMessage(username = "sam"){
+    //if(!username){
+      //  console.log("PLease enter a username");
+        //return
+    //}
+    //return `${username} just logged in`
+//}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+//function calculateCartPrice(val1, val2, ...num1){
+  //  return num1
+//}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+//const user = {
+  //  username: "hitesh",
+    //prices: 199
+//}
+
+//function handleObject(anyobject){
+  //  console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+//}
+
+// handleObject(user)
+//handleObject({
+  //  username: "sam",
+    //price: 399
+//})
+
+//const myNewArray = [200, 400, 100, 600]
+
+//function returnSecondValue(getArray){
+  //  return getArray[1]
+//}
+
+// console.log(returnSecondValue(myNewArray));
+//console.log(returnSecondValue([200, 400, 500, 1000]))
+
+function myfuncation(){
+    let a=37;
+    let b=47;
+    c =a+b;
+
+}
+
+//const readline = require('readline');
+
+//const rl = readline.createInterface({
+  //input: process.stdin,
+  //output: process.stdout
+//});
+
+//rl.question('Enter the first number: ', (num1) => {
+  //rl.question('Enter the second number: ', (num2) => {
+    // Convert input strings to numbers
+    //const number1 = parseInt(num1);
+    //const number2 = parseInt(num2);
+
+    //if (isNaN(number1) || isNaN(number2)) {
+      //console.log('Please enter valid numbers.');
+    //} else {
+      // Calculate the sum
+     // const sum = number1 + number2;
+      //console.log(`The sum of ${number1} and ${number2} is ${sum}`);
+    //}
+
+    //rl.close();
+  //});
+//});
