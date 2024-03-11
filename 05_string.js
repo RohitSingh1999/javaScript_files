49f9b636b2cd7002fbeafb32fbce304685e4ee36
Rohit Singh <154702706+RohitSingh1999@users.noreply.github.com>
2024-03-11 23:07:20 +0530

    Add files via upload

---
+++ 05_string.js
@@ -0,0 +1,37 @@
+const name = "rohit"
+const repoCount = 50
+
+ console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+ console.log(gameName);
+ console.log(gameName.__proto__);
+
+
+//const anotherString = gameName.slice(-8, 4)
+
+const newStringOne = "   rohit    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%204-3choudhary"
+
+console.log(url.replace('%204-3', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+const rohit = new String("hello world");
+console.log(rohit.toUpperCase());
+ 
+const number = "software";
+//console.log(number.toUpperCase());
+console.log(number.length);
+
+//const javas = new String("rohit, singh");
+//console.log(javas);
+
