9d66a928049401dc6f03078cc6507c6c165985a0
Rohit Singh <154702706+RohitSingh1999@users.noreply.github.com>
2024-03-11 23:06:04 +0530

    Add files via upload

---
+++ comparigion.js
@@ -0,0 +1,15 @@
+// console.log(2<4);
+// console.log(90>50);
+// console.log(70<8);
+// console.log(10<=5);
+//console.log(10<=10);
+//console.log(null>0);
+//console.log(null>=0);
+
+function myFunction() {
+    let nandini = "Rohit";
+    console.log(nandini);
+}
+
+ return myFunction(); // Call the function to log 'Rohit' to the console
+
