aa84d96ff31bc558c2e1b9d0e48a216093a9a3d9
Rohit Singh <154702706+RohitSingh1999@users.noreply.github.com>
2024-03-11 23:04:32 +0530

    Add files via upload

---
+++ Data_typeSummary.js
@@ -0,0 +1,35 @@
+//primitive data type
+// 7 type :> String, number, boolean, null, undefined,
+// symbol, bigint
+
+
+
+//Reference (Non primitive)
+
+// Array, object, funcation
+let hello = [ " hello", "rohit", "singh"]
+console.log(hello);
+
+let myfuncation = function () {
+    console.log("hello");
+}
+
+function rohit(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+rohit('John');
+
+
+const rakesh = function(){
+    const hello = "rohit";
+    console.log(hello);
+
+}
+rakesh();
+console.log(typeof rakesh);
+
+
+
+
+
